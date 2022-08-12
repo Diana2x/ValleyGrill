1453f61b2a4a89a8a9936d422b549746ec574a1c
dianaclx <dianaclxcoe@gmail.com>
2022-08-13 00:47:54 +0200

    Initial home code and styling test

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import loadWebsite from "./pages/home";
+
+loadWebsite(); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/home.js',
+    mode: "production",
+    entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
